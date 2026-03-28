8b14294de7ccf76a3443b885bd73052cd6f11210
Daliaga-c <diegoaliaga49@gmail.com>
2026-03-28 14:22:25 -0500

    feat: implementar vista de detalle

--- obj/Debug/net10.0/PortalCampanas.AssemblyInfo.cs
+++ obj/Debug/net10.0/PortalCampanas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortalCampanas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d68f3be25817b00312e69e45614ae4b4801ac5ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b49f51498a09d9344ffeeedf2c3d481980149b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortalCampanas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortalCampanas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
